a6639d28b919536a741010db97f121dac95f26ba
Cin <cin979@gmail.com>
2019-08-09 11:26:43 +1000

    Login commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,14 +5,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+  username = ["usr1", "usr2", "usr3"];
+  password = ["pass1", "pass2", "pass3"];
+
+  givenUser:string = "";
+  givenPass:string = "";
 
   constructor() {
 
-  }
+  };
 
-  ngOnInit() {
-    
-  }
+  login() {
+    var user = this.username.indexOf(this.givenUser)
+    if (user != -1){
+      if (this.password.indexOf(this.givenPass) === user){
+        alert("/src/app/account/"+this.username[user]+".html");
+      } else {
+        alert("Password Incorrect");
+      }
+    } else {
+      if (this.givenUser != "") {
+        alert("Incorrect Username");
+      } else {
+        alert("Please Enter Username");
+      }
+    }
+  };
+
+  ngOnInit() { };
 
 }
